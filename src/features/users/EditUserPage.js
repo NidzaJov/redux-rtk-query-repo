ceec934ec57ba880604dbb4b7e614eb2ef83d747
Nikola Jovanović <nidza.jov@gmail.com>
2021-12-21 23:09:53 +0100

    add CSS for EditUserPage

--- src/features/users/EditUserPage.js
+++ src/features/users/EditUserPage.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import styles from "./EditUserPage.module.css";
 
 import { useGetUserQuery, useEditUserMutation } from '../api/apiSlice';
 
@@ -50,49 +51,56 @@ export const EditUserPage = () => {
             }
         }  
 
-        return (<section>
+        return (<section className={styles.edit_user_section}>
             <h3>Edit user:</h3>
-            <form className="add-user-form">
-                <label htmlFor="name">
-                    Name: <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-                </label>
-                <label htmlFor="userName">
-                UserName: <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)}/>
-                </label>
-                <label htmlFor="email">
-                E-mail: <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                </label>
+            <form className={styles.edit_user_form}>
+                <div className={styles.edit_data_section}>
+                    <label htmlFor="name">
+                        <span>Name:</span> <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                    </label>
+                    <label htmlFor="userName">
+                    <span>UserName:</span> <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+                    </label>
+                    <label htmlFor="email">
+                    <span>E-mail:</span> <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </label>
+                </div>
                 <h4>Adress:</h4>
-                <label htmlFor="street">
-                Street: <input type="text" value={street} onChange={(e) => setStreet(e.target.value)}/>
-                </label>
-                <label htmlFor="suite">
-                Suite: <input type="text" value={suite} onChange={(e) => setSuite(e.target.value)}/>
-                </label>
-                <label htmlFor="city">
-                City: <input type="text" value={city} onChange={(e) => setCity(e.target.value)}/>
-                </label>
-                <label htmlFor="zipcode">
-                Zip code: <input type="text" value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
-                </label>
-                <label htmlFor="phone">
-                Phone: <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)}/>
-                </label>
-                <label htmlFor="website">
-                Website: <input type="text" value={website} onChange={(e) => setWebsite(e.target.value)}/>
-                </label>
+                <div className={styles.edit_data_section}>
+                    <label htmlFor="street">
+                    <span>Street:</span> <input type="text" value={street} onChange={(e) => setStreet(e.target.value)}/>
+                    </label>
+                    <label htmlFor="suite">
+                    <span>Suite:</span> <input type="text" value={suite} onChange={(e) => setSuite(e.target.value)}/>
+                    </label>
+                    <label htmlFor="city">
+                    <span>City:</span> <input type="text" value={city} onChange={(e) => setCity(e.target.value)}/>
+                    </label>
+                    <label htmlFor="zipcode">
+                    <span>Zip code:</span> <input type="text" value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
+                    </label>
+                    <label htmlFor="phone">
+                    <span>Phone:</span> <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+                    </label>
+                    <label htmlFor="website">
+                    <span>Website:</span> <input type="text" value={website} onChange={(e) => setWebsite(e.target.value)}/>
+                    </label>
+                </div>
+                
                 <h4>Company:</h4>
-                <label htmlFor="companyName">
-                Company name: <input type="text" value={companyName} onChange={(e) => setCompanyName(e.target.value)}/>
-                </label>
-                <label htmlFor="catchPhrase">
-                Company catch-phrase: <textarea value={catchPhrase} onChange={(e) => setCatchPhrase(e.target.value)}/>
-                </label>
-                <label htmlFor="bs">
-                Bs: <input type="text" value={bs} onChange={(e) => setBs(e.target.value)}/>
-                </label>
+                <div className={styles.edit_data_section}>
+                    <label htmlFor="companyName">
+                    <span>Company name:</span> <input type="text" value={companyName} onChange={(e) => setCompanyName(e.target.value)}/>
+                    </label>
+                    <label htmlFor="catchPhrase">
+                    <span>Company catch-phrase:</span> <textarea value={catchPhrase} onChange={(e) => setCatchPhrase(e.target.value)}/>
+                    </label>
+                    <label htmlFor="bs">
+                    <span>Bs:</span> <input type="text" value={bs} onChange={(e) => setBs(e.target.value)}/>
+                    </label>
+                </div>
 
-                <div>
+                <div className={styles.save_button_section}>
                     <button type="button" onClick={onSaveUserClicked} disabled={isItLoading}>Save user</button>
                 </div>
             </form>
