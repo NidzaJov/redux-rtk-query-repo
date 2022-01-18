39de413c0f170be5e1619e1d66fb77e478a10607
Nikola Jovanović <nidza.jov@gmail.com>
2022-01-18 02:37:32 +0100

    improve responsivity of navbar, try to make it position fixed, huge problem, fix it with top-margin and z-index

--- src/views/MainLayout.js
+++ src/views/MainLayout.js
@@ -49,24 +49,27 @@ export const MainLayout = (props) => {
     }
 
     return (
-        <div>
-            <div ref={navigationDiv} className={styles.navigation_div}>
-                <button onClick={goToUsers} className={activeButton === 'users'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="users"/> Users</button>
-                <button onClick={goToApendix} className={activeButton === 'apendix'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="paperclip"/> Apendix</button>
-                <button onClick={goToMedia} className={activeButton === 'media'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon='photo-video'/> Media</button>
-                <button onClick={goToSvg} className={activeButton === 'svg'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="bezier-curve"/> SVG</button>
-                <button onClick={goToTodos} className={activeButton === 'todos'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="clipboard-check"/> Todos</button>
-                <div className={styles.search_container}>
-                    <form>
-                        <input type="text" placeholder="Search..." name="search"></input>
-                        <button type="submit" className={styles.search_button}><FontAwesomeIcon icon="search"></FontAwesomeIcon></button>
-                    </form>
-                    
-                </div>
+        <div className={styles.main_content}>
+            <div className={styles.fixed_nav_div}>
+                <div ref={navigationDiv} className={styles.navigation_div}>
+                    <button onClick={goToUsers} className={activeButton === 'users'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="users"/> Users</button>
+                    <button onClick={goToApendix} className={activeButton === 'apendix'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="paperclip"/> Apendix</button>
+                    <button onClick={goToMedia} className={activeButton === 'media'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon='photo-video'/> Media</button>
+                    <button onClick={goToSvg} className={activeButton === 'svg'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="bezier-curve"/> SVG</button>
+                    <button onClick={goToTodos} className={activeButton === 'todos'? styles.active_button : styles.nav_button}><FontAwesomeIcon icon="clipboard-check"/> Todos</button>
+                    <div className={styles.search_container}>
+                        <form>
+                            <input type="text" placeholder="Search..." name="search"></input>
+                            <button type="submit" className={styles.search_button}><FontAwesomeIcon icon="search"></FontAwesomeIcon></button>
+                        </form>
+                        
+                    </div>
 
-                <button className={styles.icon} onClick={showVerticalNav}><FontAwesomeIcon  icon={faBars} /></button>
+                    <button className={styles.icon} onClick={showVerticalNav}><FontAwesomeIcon  icon={faBars} /></button>
+                </div>
             </div>
-            <div>
+            
+            <div className={styles.children}>
                 {props.children}
             </div>
         </div>
