1701418367f462c602bdebe28dc3881f8bc3baa9
Nikola Jovanović <nidza.jov@gmail.com>
2022-01-19 01:00:32 +0100

    Apply dynamic fixed position placehoders in scroling list of tods, refactor some css

--- src/features/todos/TodosList.js
+++ src/features/todos/TodosList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { selectAllTodos} from './todosSlice';
 import styles from './TodosList.module.css';
@@ -6,21 +6,20 @@ import styles from './TodosList.module.css';
 export const TodosList = () => {
     const todos = useSelector(selectAllTodos);
     
-
     const LiItem = ({ todo }) => {
         const [checkBoxStyle, setCheckBoxStyle] = useState({ display: 'none'});
-    const [liStyle, setLiStyle] = useState({ 
-        backgroundColor: '#04AA6D',
-        padding: '15px',
-        width: '50%',
-        fontSize: '14px',
-        color: 'white',
-        borderRadius: '0 5px 5px 0',
-        display: 'flex',
-        justifyContent: 'space-between',
-        marginBottom: '10px',
-        transition: '0.3s'
-    })
+        const [liStyle, setLiStyle] = useState({ 
+            backgroundColor: '#04AA6D',
+            padding: '15px',
+            width: '50%',
+            fontSize: '14px',
+            color: 'white',
+            borderRadius: '0 5px 5px 0',
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: '10px',
+            transition: '0.3s'
+        })
 
     const useHoverStyle = () => {
         setLiStyle(style => { 
@@ -58,11 +57,72 @@ export const TodosList = () => {
             <LiItem todo={todo}></LiItem>   
         </li>
     ))
+    
+    const [upperExcessBarStyle, setUpperExcessBarStyle] = useState({
+        display: 'none',
+        position: 'absolute',
+        top: '0px',
+        textAlign: 'center',
+        width: '100%',
+        transition: '0.3s',
+    });
+    const [lowerExcessBarStyle, setLowerExcessBarStyle] = useState({
+        display: 'block',
+        position: 'absolute',
+        bottom: '186px',
+        textAlign: 'center',
+        width: '100%',
+        transition: '0.3s',
+    } );
+    const scrollingElement = useRef();
+
+    
+    const showExcessBars = () => {
+        console.log('Scrolling');
+        
+        console.log('Styles:', upperExcessBarStyle, lowerExcessBarStyle);
+        const scrollBottom = scrollingElement.current.scrollHeight - scrollingElement.current.scrollTop;
+        console.log(scrollingElement.current.scrollTop, scrollBottom);
+        if (scrollingElement.current.scrollTop > 60 && scrollingElement.current.scrollTop < 250) {
+            setUpperExcessBarStyle(style => {
+                return { ...style, display: 'block'}
+            });
+            setLowerExcessBarStyle(style => {
+                return { ...style, display: 'block'}
+            });
+        } else if (scrollingElement.current.scrollTop > 250) {
+            setUpperExcessBarStyle(style => {
+                return { ...style, display: 'block'}
+            });
+            setLowerExcessBarStyle(style => {
+                return { ...style, display: 'none'}
+            });
+           
+        } else if (scrollingElement.current.scrollTop < 60) {
+            setUpperExcessBarStyle(style => {
+                return { ...style, display: 'none'}
+            });
+            setLowerExcessBarStyle(style => {
+                return { ...style, display: 'block'}
+            });
+        }
+        
+    }
+    
 
     return (
         <section>
             <h2>Todos</h2>
-            <ul className={styles.todos_list}>{renderedTodos}</ul>
+            <div  className={styles.scrolling_container}
+                >
+                <div style={upperExcessBarStyle}>More todos...</div>
+                <ul  ref={scrollingElement} className={styles.todos_list}
+                onScroll={showExcessBars} >
+                    {renderedTodos}
+                </ul>
+                <div  style={lowerExcessBarStyle}>More todos...</div>
+            </div>
+            
         </section>
     )
 }
